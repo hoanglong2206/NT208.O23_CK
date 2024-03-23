6609fa9b7a9fc1ba91e1595afd4f9a34fc036de5
ttn0321 <thanhnhanta03@gmail.com>
2024-03-24 04:02:24 +0700

    Search Bar Completed

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import {format} from "date-fns"
 
-const Header = () => {
+const Header = ({type}) => {
     const [openDate, setOpenDate]=useState(false)
     const [date, setDate] = useState([
         {
@@ -22,9 +22,14 @@ const Header = () => {
         children:0,
         room:1,
     });
+    const handleOption=(name,operation)=>{
+        setOptions(prev=>{return{
+            ...prev,[name]:operation==="i" ? options[name] + 1 : options[name] - 1,
+        }})
+    }
     return (
         <div className="header">
-            <div className="headerContainer">  
+            <div className={type === "list" ? "headerContainer listMode" : "headerContainer"}>  
                 <div className="headerList">
                     <div className="headerListItem active">
                         <FontAwesomeIcon icon={faBed}/>
@@ -47,6 +52,8 @@ const Header = () => {
                         <span>Airport taxis</span>
                     </div>
                 </div>
+                { type !== "list" &&
+                    <>
                 <h1 className="headerTitle">Sign in, save money</h1>
                 <p className="headerDesc">
                     Save 10% or more at participating properties – just look for the blue Genius label.
@@ -74,12 +81,39 @@ const Header = () => {
                     </div>
                     <div className="headerSearchItem">
                         <FontAwesomeIcon icon={faPerson} className="headerIcon"/>
-                        <span className="headerSearchText">2 adults 2 children 1 room</span>
+                        <span onClick={()=>setOpenOptions(!openOptions)}className="headerSearchText">{`${options.adult} adult · ${options.children} children · ${options.room} room`}</span>
+                            {openOptions && <div className="options">
+                                <div className="optionItem">
+                                    <span className="optionText">Adult</span>
+                                    <div className="optionCounter">
+                                        <button disabled={options.adult<=1} className="optionCounterButton" onClick={()=>handleOption("adult","d")}
+                                        >-</button>
+                                        <span className="optionCounterNumber">{options.adult}</span>
+                                        <button className="optionCounterButton" onClick={()=>handleOption("adult","i")}>+</button>
+                                    </div>
+                                </div>
+                                <div className="optionItem">
+                                    <span className="optionText">Children</span>
+                                    <div className="optionCounter">
+                                        <button disabled={options.children<=0} className="optionCounterButton" onClick={()=>handleOption("children","d")}>-</button>
+                                        <span className="optionCounterNumber">{options.children}</span>
+                                        <button className="optionCounterButton" onClick={()=>handleOption("children","i")}>+</button>
+                                    </div>
+                                </div>
+                                <div className="optionItem">
+                                    <span className="optionText">Room</span>
+                                    <div className="optionCounter">
+                                        <button disabled={options.room<=1} className="optionCounterButton" onClick={()=>handleOption("room","d")}>-</button>
+                                        <span className="optionCounterNumber">{options.room}</span>
+                                        <button className="optionCounterButton" onClick={()=>handleOption("room","i")}>+</button>
+                                    </div>
+                                </div>
+                            </div>}
                     </div>
                     <div className="headerSearchItem">
                         <button className="headerBtn">Search</button>
                     </div>
-                </div>
+                </div></>}
             </div>
         </div>
       )
--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
 
-const Hotel = () => {
-  return (
-    <div>Hotel</div>
+const Hotel = () =>{
+  return(
+      <div>
+          Hotel
+      </div>
   )
 }
 
