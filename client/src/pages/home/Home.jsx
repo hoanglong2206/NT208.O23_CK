86efdadcac098dfc024c5b5014089e6a4cdf5b56
ttn0321 <thanhnhanta03@gmail.com>
2024-03-24 05:40:35 +0700

    Email List

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@
 import Featured from "../../components/featured/Featured"
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/header/Header"
+import MailList from "../../components/mailList/MailList"
 import Navbar from "../../components/navbar/Navbar"
 import PropertyList from "../../components/propertyList/PropertyList"
 import "./home.css"
@@ -17,6 +18,7 @@ const Home=()=>{
                 <PropertyList/>
                 <h1 className="homeTitle">Homes guests love</h1>
                 <FeaturedProperties/>
+                <MailList/>
             </div>
         </div>
     )
