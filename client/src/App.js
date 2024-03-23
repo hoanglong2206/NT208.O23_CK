566723298026f36593b4999eee3f64041c75ca00
hoanglong2206 <21522307@gm.uit.edu.vn>
2024-03-23 20:51:27 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from './pages/home/Home';
-import List from './pages/list/List';
-import Hotel from './pages/hotel/Hotel';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/hotels" element={<List/>}/>
-        <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
       </Routes>
     </BrowserRouter>
   );
