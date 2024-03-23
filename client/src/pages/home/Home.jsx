e882a4c296e1a7e8244c7ddf8c022aa729f94539
ttn0321 <thanhnhanta03@gmail.com>
2024-03-24 05:48:16 +0700

    Footer

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 
 import Featured from "../../components/featured/Featured"
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
+import Footer from "../../components/footer/Footer"
 import Header from "../../components/header/Header"
 import MailList from "../../components/mailList/MailList"
 import Navbar from "../../components/navbar/Navbar"
@@ -19,6 +20,7 @@ const Home=()=>{
                 <h1 className="homeTitle">Homes guests love</h1>
                 <FeaturedProperties/>
                 <MailList/>
+                <Footer/>
             </div>
         </div>
     )
